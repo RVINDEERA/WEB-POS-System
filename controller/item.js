287f8fc48eeaf28b152daa74af67bd82671c7108
RVINDEERA <rdasbandara@gmail.com>
2024-06-14 22:53:54 +0530

    crud

--- controller/item.js
+++ controller/item.js
@@ -1,4 +1,7 @@
+import ItemModel from "../model/ItemModel.js";
+import {items} from "../db/db.js";
 
+var recordIndexItems;
 /*
 $('#dashboard-section').css({display: 'none'});
 $('#customer-section').css({display: 'none'});
@@ -6,3 +9,5 @@ $('#item-section').css({display: 'block'});
 $('#order-section').css({display: 'none'});
 $('#placeOrder-section').css({display: 'none'});
 */
+
+
