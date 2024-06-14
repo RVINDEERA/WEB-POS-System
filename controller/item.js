c4c720e7535809294daf8b55ce81fd1361b70196
RVINDEERA <rdasbandara@gmail.com>
2024-06-14 23:04:50 +0530

    validation part

--- controller/item.js
+++ controller/item.js
@@ -10,4 +10,78 @@ $('#order-section').css({display: 'none'});
 $('#placeOrder-section').css({display: 'none'});
 */
 
+var ValidItemID = $('#item-code>#item-code');
+var ValidItemName = $('#item-name>#item-name');
+var ValidPrice = $('#item-unitprice>#item-unitprice');
+var ValidQty = $('#item-qty>#item-qty');
+var isValidPriceAndQty = new RegExp("^[0-9]+\\.?[0-9]*$");
+var isValidItemName = new RegExp("\\b[A-Z][a-z]*( [A-Z][a-z]*)*\\b");
+var isValidCode = new RegExp("I\\d\\d\\d");
 
+
+function emptyPlaceHolder() {
+    $(ValidItemID).attr("placeholder", "");
+    $(ValidItemName).attr("placeholder", "");
+    $(ValidPrice).attr("placeholder", "");
+    $(ValidQty).attr("placeholder", "");
+}
+function clearAll() {
+    $('#item-code').val("");
+    $('#item-name').val("");
+    $('#item-unitprice').val("");
+    $('#item-qty').val("");
+}
+
+
+function validItem() {
+    var itemID = $('#item-code').val();
+    var itemName = $('#item-name').val();
+    var itemPrice = $('#item-unitprice').val();
+    var itemQty = $('#item-qty').val();
+
+
+    if (!isValidCode.test(itemID) || !isValidItemName.test(itemName) || !isValidPriceAndQty.test(itemPrice) || !isValidPriceAndQty.test(itemQty)) {
+
+        $(ValidItemID).attr("placeholder", "ID Empty");
+        $(ValidItemName).attr("placeholder", "Wrong Input Try Again");
+        $(ValidPrice).attr("placeholder", "Wrong Input");
+        $(ValidQty).attr("placeholder", "Wrong Input Try Again");
+
+        return true;
+    } else {
+        emptyPlaceHolder();
+    }
+}
+
+$('#item-cancel').on('click',() => {
+    clearAll();
+});
+
+function loadItemTable() {
+    $("#item-table-body").empty();
+
+    items.map((item,index) => {
+        var itemRecord = `<tr>
+                        <td class="i-id">${item.id}</td>
+                        <td class="i-name">${item.name}</td>
+                        <td class="i-price">${item.price}</td>
+                        <td class="i-qty">${item.qty}</td>
+                    </tr>`
+        $('#item-table-body').append(itemRecord);
+    });
+}
+
+
+$('#item-table-body').on('click','tr',function () {
+    recordIndexItems = $(this).index();
+
+    var id = $(this).find(".i-id").text();
+    var name = $(this).find(".i-name").text();
+    var price = $(this).find(".i-price").text();
+    var qty = $(this).find(".i-qty").text();
+
+    $('#item-code').val(id);
+    $('#item-name').val(name);
+    $('#item-unitprice').val(price);
+    $('#item-qty').val(qty);
+});
